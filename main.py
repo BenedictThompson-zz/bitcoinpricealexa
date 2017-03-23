7e7ac5ebafabff5f6a5a340a1a5d0fcc279cf777
Ben Thompson <thompson.benedictjames@gmail.com>
2017-03-23 21:26:25 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+ #!/usr/bin/python
+ # -*- coding: utf-8 -*-
+
+import logging
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question, session
+import coinmarketcap
+import os
+import sys
+reload(sys)
+crypto = 'bitcoin'
+sys.setdefaultencoding('utf8')
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+quitkeywords = ['quit', 'exit', 'cancel', 'leave']
+
+@ask.launch
+
+def new_game():
+    print "here"
+
+    welcome_msg = render_template('welcome')
+
+    return question(welcome_msg)
+
+
+@ask.intent("YesIntent", convert={'yon': str})
+def next_round(yon):
+    print yon
+    round_msg = render_template('round_msg')
+    help = render_template('help', crypto = crypto)
+    if yon == 'help':
+        print "here"
+        return question(help)
+    if yon in quitkeywords:
+        return statement("")
+    return question(round_msg)
+
+        
+
+@ask.intent("AnswerIntent")
+
+def answer():
+        price = coinmarketcap.price(crypto)
+        priceresponse = render_template('priceresponse', crypto = crypto , price = price)
+        return statement(priceresponse) \
+            .simple_card(title='Bitcoin Price', content="The " + crypto + " price is " + price)
+@ask.session_ended
+def session_ended():
+    return statement("")
+def run():
+    app.run(debug=True)
